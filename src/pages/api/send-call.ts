58c456a461acee47b50be9a79d93326f3eb80dd2
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-19 15:04:18 -0400

    building out handler function, destructured email and githubRepoUrl from body to use in data validation check

--- src/pages/api/send-call.ts
+++ src/pages/api/send-call.ts
@@ -1,1 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+
+export interface RequestBody {
+  email: string
+  githubRepoUrl: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // check to see if Post method is used
+  if (req.method === 'POST') {
+    try {
+      // destructure email and githubRepoUrl from request body
+      const { email, githubRepoUrl }: RequestBody = req.body
+
+      // check to ensure required values are provided
+      if (!email || !githubRepoUrl) {
+        return res
+          .status(400)
+          .json({ error: 'Email and Github Repo Url are both required' })
+      }
+    } catch {}
+  }
+}
