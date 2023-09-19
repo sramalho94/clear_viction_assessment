98a0bdc3e8eaadbbdc0349a0e66b2d7e60c45585
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-19 15:48:50 -0400

    imported Form component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+'use client'
+import Form from '@/components/Form'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center ">
-      <p>Hello World</p>
+      <Form />
     </main>
   )
 }
